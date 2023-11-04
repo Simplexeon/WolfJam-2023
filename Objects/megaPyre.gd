72b6c08ec6eea0cc17d7f5d83fd95c9bd1e31709
Simplexeon <gamerguy941mc@gmail.com>
2023-11-04 14:44:11 -0400

    Fixed Mega Pyre

--- Objects/megaPyre.gd
+++ Objects/megaPyre.gd
@@ -1,8 +1,8 @@
-extends StaticBody2D
+extends Node2D
 
 var current_scale : float = 0.5;
 var max_scale : float = 2.5;
-var dim_rate : float = 0.07;
+var dim_rate : float = 0.04;
 var pyre_increase_rate : float = 1.05;
 
 # Components
@@ -10,15 +10,11 @@ var pyre_increase_rate : float = 1.05;
 @onready var audio_player : AudioStreamPlayer2D = $AudioStreamPlayer2D;
 @onready var area_2d : CollisionShape2D = $Area2D/CollisionShape2D;
 
-
-var mega_pyre_file : PackedScene = preload("res://Objects/megaPyre.tscn");
-
 var shrinking : bool = false;
 
 func _ready() -> void:
 	print("here")
 	light.texture_scale = lerp(0.0, max_scale, current_scale);
-	area_2d.shape.radius = 323 * current_scale;
 
 
 func _physics_process(delta: float) -> void:
@@ -30,10 +26,11 @@ func _physics_process(delta: float) -> void:
 	light.texture_scale = lerp(0.0, max_scale, current_scale);
 	current_scale -= dim_rate * delta;
 	audio_player.max_distance = 200 * current_scale;
-	area_2d.shape.radius = 323 * current_scale;
 	if(current_scale < 0.05):
-		var next_pyre : StaticBody2D = mega_pyre_file.instantiate();
-		var parent : Node2D = get_parent().get_parent().get_child(randi_range(0, 5));
+		var next_pyre : Node2D = load("res://Objects/megaPyre.tscn").instantiate();
+		var parent : Node2D;
+		while(parent == get_parent() or parent == null):
+			parent = get_parent().get_parent().get_child(randi_range(0, 5));
 		parent.add_child(next_pyre);
 		next_pyre.global_position = parent.global_position;
 		queue_free();
@@ -49,11 +46,3 @@ func _on_audio_stream_player_2d_finished() -> void:
 	audio_player.play();
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.enemy_dead.connect(_fed_fire);
-
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player:
-		body.enemy_dead.disconnect(_fed_fire);
